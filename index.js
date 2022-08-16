34a6e204a1843f871eecf500a2202a91eea684a3
Jacob <jacobamelse@hotmail.com>
2022-08-15 20:51:52 -0500

    commit tree 06d6b5a5549e246aa8b4729d9091cac32f449af2

--- index.js
+++ index.js
@@ -9,15 +9,77 @@
  for you to use if you need it!
  */
 
+ const createEmployeeRecord = (array) => {
+    return {
+      firstName: array[0],
+      familyName: array[1],
+      title: array[2],
+      payPerHour: array[3],
+      timeInEvents: [],
+      timeOutEvents: []
+      }
+    }
+
+const createEmployeeRecords = function (array){
+    return array.map(record => createEmployeeRecord(record));
+    }
+
+
+const createTimeInEvent = function (dateStamp){
+    const [date, hour] = dateStamp.split(' ')
+    const newInRecord = {
+        type: 'TimeIn',
+        hour: parseInt(hour),
+        date: date
+      }
+    this.timeInEvents.push(newInRecord)
+    
+    return this
+}
+
+const createTimeOutEvent = function (dateStamp){
+    const [date, hour] = dateStamp.split(' ')
+    const newOutRecord = {
+        type: 'TimeOut',
+        hour: parseInt(hour),
+        date: date
+      }
+    this.timeOutEvents.push(newOutRecord)
+    return this
+}
+
+
+const hoursWorkedOnDate = function (targetDate){
+    const inE = this.timeInEvents.find(iEv => iEv.date === targetDate)
+    const outE = this.timeOutEvents.find(oEv => oEv.date === targetDate)
+    return (outE.hour - inE.hour) / 100;
+}
+
+
+const wagesEarnedOnDate = function (targetDate){
+    return hoursWorkedOnDate.call(this, targetDate) * this.payPerHour
+}
+
+
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
     })
 
-    const payable = eligibleDates.reduce(function (memo, d) {
+const payable = eligibleDates.reduce(function (memo, d) {
         return memo + wagesEarnedOnDate.call(this, d)
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
 }
 
+const findEmployeeByFirstName = function(array, name){
+    return array.find(record => record.firstName === name)
+ }
+
+
+ const calculatePayroll = function(array){
+    return array.reduce((value, record) => {
+        return value + allWagesFor.call(record)
+    }, 0)
+ }
